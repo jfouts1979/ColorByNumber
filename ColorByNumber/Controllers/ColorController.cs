9944563788dd811abd2ac8783b75afa1c1f848e5
John R. Fouts, MBA <Fouts.John@gmail.com>
2018-06-07 21:41:06 -0400

    Added a way to get unique values from existing arrays.

---
+++ ColorByNumber/Controllers/ColorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColorByNumber.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: GetColorInfo
+        public ActionResult GetColorInfo()
+        {
+            return View();
+        }
+    }
+}
